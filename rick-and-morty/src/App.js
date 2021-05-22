968a68f8277f0cb9977162704fb3576fec40c3dd
parmam <38482561+parmam@users.noreply.github.com>
2021-05-22 16:16:37 -0300

    'asd'

--- rick-and-morty/src/App.js
+++ rick-and-morty/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.png';
+import React from 'react';
+import { Route } from "react-router-dom";
+
+
+//components
+import Nav from './components/Nav/Nav'
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <React.Fragment>
+        <Nav />
+    </React.Fragment>
   );
 }
 
--- rick-and-morty/src/components/About/About.jsx
+++ rick-and-morty/src/components/About/About.jsx
@@ -1,1 +1,19 @@
 import React from "react"
+
+import styles from './About.module.css'
+
+export default function Nav(){
+
+
+
+
+
+
+
+    return(
+        <React.Fragment>
+            lalala
+        </React.Fragment>
+    )
+
+}
