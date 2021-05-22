5cdc3e93687613f8e8a8c2c192a00aa9355baa7e
Agustin Gallego <gallegoagustin7@gmail.com>
2021-05-22 18:19:17 -0300

    Hecho Redux

--- rick-and-morty/src/index.js
+++ rick-and-morty/src/index.js
@@ -5,15 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
