3d6ae3550d7f4eb20e2229ddcfa9d7c15fde3c3e
Tomas Roquinaj <roquinaj.t@hotmail.com>
2021-05-22 13:59:48 -0300

    x

--- rick-and-morty/src/components/Card/Card.jsx
+++ rick-and-morty/src/components/Card/Card.jsx
@@ -1,9 +1,14 @@
 import React from "react"
+import styles from "./Card.module.css"
  // foto y nombre del personaje 
 
- function Card () {
+ function Card (props) {
+    const {name, img} = props
      return 
-  
+  <div> 
+      <span> {name}</span>
+      <img src={`http://${img}`}/>
+  </div>
 
 
 
