d8c63aa4769284edd0dccc95cc480de51cb50f12
parmam <38482561+parmam@users.noreply.github.com>
2021-05-23 18:21:22 -0300

    parmadev/navConROuting

--- rick-and-morty/src/App.js
+++ rick-and-morty/src/App.js
@@ -3,16 +3,21 @@ import { Route } from "react-router-dom";
 
 //components
 import Buscador from "./components/Buscador/Buscador"
-
+import Nav from "./components/Nav/Nav"
+import About from './components/About/About'
+import Favoritos from './components/Favoritos/Favoritos'
 
 
 import './App.css';
 
 function App() {
   return (
-    <>
+    <React.Fragment>
+      <Nav />
       <Route exact path="/" component={Buscador} />
-    </>
+      <Route path="/about" component={About}/>
+      <Route path="/favoritos" component={Favoritos}/>
+    </React.Fragment>
   );
 }
 
--- rick-and-morty/src/components/About/About.jsx
+++ rick-and-morty/src/components/About/About.jsx
@@ -4,7 +4,9 @@ import styles from './About.module.css'
 
     function About(){
         return(
-            <div></div>
+            <React.Fragment>
+                about
+            </React.Fragment>
         )
 }
 
