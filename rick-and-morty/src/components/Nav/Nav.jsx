f95efa7fd757b6a5ba9e0baa27867a6af0133bed
parmam <38482561+parmam@users.noreply.github.com>
2021-05-24 01:56:43 -0300

    navStylesV1

--- rick-and-morty/src/components/Nav/Nav.jsx
+++ rick-and-morty/src/components/Nav/Nav.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { NavLink } from 'react-router-dom';
 
 import styles from "./Nav.module.css"
-
+import navLogo from '../../assets/img/rick-and-morty-logo.png'
 
 import About from "../About/About"
 
@@ -10,16 +10,26 @@ export default function Nav(){
     return(
             <div className={styles.navContainer}>
                 <div className={styles.iContainer}>
-
+                    <img className={styles.navLogo} src={navLogo} alt="" srcset="" />
                 </div>
                 <div className={styles.navbarContainer}>
                     <ul className={styles.navbar}>
-                        <li className={styles.navbarItem}>
-                            <NavLink to="/favoritos">Favoritos</NavLink>
-                        </li>                    
-                        <li className={styles.navbarItem}>
-                            <NavLink to="/about">About</NavLink></li>
-                        <li className={styles.navbarItem}>settings</li>
+                        <NavLink  to="/">                 
+                            <li className={styles.navbarItem}>
+                               <span className={styles.itemText}>Inicio</span>
+                            </li>    
+                        </NavLink>
+                        <NavLink to="/favoritos">
+                            <li className={styles.navbarItem}>
+                               <span className={styles.itemText}>Favoritos</span>
+                            </li>  
+                        </NavLink>
+                        <NavLink to="/about">    
+                            <li className={styles.navbarItem}>
+                                <span className={styles.itemText}>About</span>
+                            </li>
+                        </NavLink>
+
                     </ul>
 
                 </div>
