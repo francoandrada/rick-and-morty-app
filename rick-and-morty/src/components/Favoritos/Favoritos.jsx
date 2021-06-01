fdcc9d70c82b6555ae47dee0f61cc9091d060fc7
Tomas Roquinaj <roquinaj.t@hotmail.com>
2021-05-31 22:09:03 -0300

    Estilos fav y home

--- rick-and-morty/src/components/Favoritos/Favoritos.jsx
+++ rick-and-morty/src/components/Favoritos/Favoritos.jsx
@@ -5,28 +5,35 @@ import styles from './Favoritos.module.css'
 
 function Favoritos(props) {
     return (
-        <div class={styles.divfav}>
-            <h3 id= {styles.title}> Tus Personajes Favoritos</h3>
-            {
-                props.chars.map(char => (
-                    <div class={styles.card} >
 
-                        <img class={styles.cardImage} src={char.image} alt={char.name} />
+        <React.Fragment>
 
-                        
-                        <div className={styles.textContainer} >
-                            <p>Gender:{char.gender} </p>
-                            <p>Name:{char.name} </p>
-                            <p>Species:{char.species} </p>
+            <div class={styles.divfav}>
+                <h3 class={styles.title}> Tus Personajes Favoritos</h3>
+            <div class= {styles.cardContainer}>
+                {
+                    props.chars.map(char => (
+                        <div class={styles.card} >
 
+                            <img class={styles.cardImage} src={char.image} alt={char.name} />
+
+
+                            <div className={styles.textContainer} >
+                                <p>Gender:{char.gender} </p>
+                                <p>Name:{char.name} </p>
+                                <p>Species:{char.species} </p>
+
+                            </div>
+                            <div id={styles.removeButton}>
+                                <button id={styles.btn} onClick={() => props.removeFavorite({ id: char.id })}> x </button>
+                            </div>
                         </div>
-                        <div id={styles.removeButton}>
-                            <button id= {styles.btn} onClick={() => props.removeFavorite({ id: char.id })}> x </button>
-                        </div>
-                    </div>
-                ))
-            }
-        </div>
+                    ))
+                }
+            </div>
+                
+            </div>
+        </React.Fragment>
     )
 }
 
