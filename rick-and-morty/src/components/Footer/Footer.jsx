bf85aab6e57b78c9862f9f7e88fa71c2c1f139ed
francoandrada <francoarganiaraz@gmail.com>
2021-06-01 01:14:59 -0300

    Footer

--- rick-and-morty/src/components/Footer/Footer.jsx
+++ rick-and-morty/src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 
 import styles from './Footer.module.css'
+import { AiFillGithub } from 'react-icons/ai';
 
     function Footer(){
         return(
-            <div></div>
+            <div className={styles.container}>
+                <div className={styles.containerIconGit}>
+                    <a href="https://github.com/francoandrada/rick-and-morty-app" target = "_ blank" className={styles.link}><AiFillGithub className={styles.iconGit}/> </a>
+                </div>
+            </div>
         )
 }
 
